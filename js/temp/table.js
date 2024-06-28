2eed974c03227ea875ca790ff6c5810b75cd22f0
Yoginara Pratama Sitorus <114574855+Yoginara@users.noreply.github.com>
2024-06-28 14:55:22 +0700

    update edit delete

--- js/temp/table.js
+++ js/temp/table.js
@@ -6,5 +6,15 @@ export let isiTabel = `<tr>
       <td style="width: 140px">#TINGGI#</td>
       <td style="width: 140px">#KELAMIN#</td>
       <td style="width: 140px">#HARGA#</td>
-      <td style="width: 140px"><button>View</button></td>
-    </tr>`
+      <td style="width: 140px">
+    <div class="flex items-center justify-around">
+      <button class="edit-btn text-blue-500">
+        <i class="ri-edit-2-line"></i>
+      </button>
+      <div class="separator"></div>
+      <button class="delete-btn text-red-500">
+        <i class="ri-delete-bin-line"></i>
+      </button>
+    </div>
+  </td>
+    </tr>`;
