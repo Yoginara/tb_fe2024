3b9ca7ce7784630c7f6d8b89076ba9d6f47b696d
agung6544 <agung.nainggolan8008@gmail.com>
2024-07-01 23:48:31 +0700

    updateinsertayam

--- js/controller/post.js
+++ js/controller/post.js
@@ -1,15 +1,22 @@
 import { postData } from "https://bukulapak.github.io/api/process.js";
 import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
-import { urlInsAyam, AmbilResponse} from "../config/url.js";
+import { urlInsAyam} from "../config/url.js";
+import { AmbilResponse} from "../config/response.js";
 
 function pushData(){
     let data = {
-        jenis : parseFloat(getValue("jenis")),
-        umur : parseFloat(getValue("umur")),
-        bobot : getValue("bobot"),
-        tinggi : getValue("tinggi"),
-        jenis_kelamin : getValue("jenis_kelamin"),
-        harga : getValue("harga"),
+        jenis: getValue("jenis"),
+        umur: getValue("umur"),
+        bobot: getValue("bobot"),
+        tinggi: getValue("tinggi"),
+        jenis_kelamin: getValue("jenis_kelamin"),
+        harga: getValue("harga"),
+    };
+
+    // Validate if required fields are filled correctly
+    if (!data.jenis || isNaN(data.umur) || isNaN(data.bobot) || isNaN(data.tinggi) || !data.jenis_kelamin || isNaN(data.harga)) {
+        alert("Please fill out all fields with valid data.");
+        return;
     }
     postData(urlInsAyam, data, AmbilResponse);
 }
